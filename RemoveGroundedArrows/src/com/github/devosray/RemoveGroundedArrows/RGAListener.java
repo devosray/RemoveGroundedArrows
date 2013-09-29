4f18bed263c4bbafe3a07a35cbc32f2e88f91678
Raymond de Vos <devosray@gmail.com>
2013-09-29 16:53:35 +0200

    Basic arrow-shooting detection

--- RemoveGroundedArrows/src/com/github/devosray/RemoveGroundedArrows/RGAListener.java
+++ RemoveGroundedArrows/src/com/github/devosray/RemoveGroundedArrows/RGAListener.java
@@ -4,7 +4,11 @@
  */
 package com.github.devosray.RemoveGroundedArrows;
 
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 
 /**
  *
@@ -16,6 +20,17 @@ public class RGAListener implements Listener {
     
     public RGAListener(RGAMain main){
         this.main = main;
+    }
+    
+    @EventHandler
+    public void onProjectileLaunchEvent(ProjectileLaunchEvent evt){
+        //When an arrow is shot, add it to an array for ground/collision check
+        
+        //Check if it is not a player-shot arrow
+        if (!(evt.getEntity().getShooter() instanceof Player) && evt.getEntity() instanceof Arrow){
+            main.getLogger().info("An arrow has been shot!");
+            main.addArrow((Arrow) evt.getEntity());
+        }
     }
     
 }
--- RemoveGroundedArrows/src/com/github/devosray/RemoveGroundedArrows/RGAMain.java
+++ RemoveGroundedArrows/src/com/github/devosray/RemoveGroundedArrows/RGAMain.java
@@ -6,16 +6,45 @@ package com.github.devosray.RemoveGroundedArrows;
  * @author Raymond
  */
 
+import java.util.ArrayList;
+import org.bukkit.Server;
+import org.bukkit.entity.Arrow;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RGAMain extends JavaPlugin{
  
     private RGAListener listener = new RGAListener(this);
+    private int repeatID;
+    private ArrayList<Arrow> arrows;
     
     @Override
     public void onEnable(){
         //Register listener
         getServer().getPluginManager().registerEvents(listener, this);
+        
+        //init arrows
+        arrows = new ArrayList<>();
+        
+        //Repeating task to check on fired arrows
+        repeatID = getServer().getScheduler().scheduleSyncRepeatingTask(this,
+                new Runnable() {
+
+            @Override
+            public void run() {
+                
+            }
+        }, 0, 1);
+    }
+    
+    @Override
+    public void onDisable(){
+        //Stop recurring task
+        getServer().getScheduler().cancelTask(repeatID);
+    }
+    
+    public void addArrow(Arrow arw){
+        //Add arrow to lists of arrows to be checked
+        arrows.add(arw);
     }
     
 }
